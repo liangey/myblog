ff0158a6354a8607b7ec1e840e1b06c2d41eae6c
liangey <291406980@qq.com>
2018-12-26 17:48:20 +0800

    ley

---
+++ blog/blog_urls.py
@@ -0,0 +1,14 @@
+#Author:ley
+# --*-- coding: utf-8 --*--
+#@Time : 2018-12-26 17:33
+#@Author: ley
+#@Site : 
+#@File : blog_urls.py
+#@Software : PyCharm
+
+from django.conf.urls import  url
+from . import  views
+
+urlpatterns=[
+    url(r'^index/',views.index),
+]
--- myblog/urls.py
+++ myblog/urls.py
@@ -14,10 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 from blog.views import index
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('index/', index)
+    #path('index/', index)
+    path(r'^index/', include('blog.blog_urls')),
 ]
